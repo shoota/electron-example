966ed6b50e9a00bca71235710266fbf334663260
Shoota Kumano <shoota.dev@gmail.com>
2017-03-22 23:53:55 +0900

    add menu and fullscreen

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 var electron = require('electron');
 var app = electron.app;
 var BrowserWindow = electron.BrowserWindow;
-
+var Menu = electron.Menu;
 var mainWindow = null;
+var remote = require("electron").remote;
 
 app.on('window-all-closed', function() {
   if (process.platform != 'darwin')
@@ -12,12 +13,39 @@ app.on('window-all-closed', function() {
 });
 
 app.on('ready', function() {
+  // メニューをアプリケーションに追加
+  Menu.setApplicationMenu(menu);
+  openWindow();
+});
 
-  // ブラウザ(Chromium)の起動, 初期画面のロード
+function openWindow() {
   mainWindow = new BrowserWindow({width: 800, height: 600});
   mainWindow.loadURL('file://' + __dirname + '/index.html');
-
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
-});
+}
+
+function fullscreen(){
+  var w = BrowserWindow.getFocusedWindow();
+  w.setFullScreen(!w.isFullScreen());
+}
+
+
+var template = [
+  {
+    label: 'ReadUs',
+    submenu: [{label: 'Quit', accelerator: 'Command+Q', click: function () {app.quit();}}]
+  },
+
+  {
+    label: 'View',
+    submenu: [
+      { label: 'Reload', accelerator: 'Command+R', click: function() { BrowserWindow.getFocusedWindow().reload(); } },
+      { label: 'Toggle DevTools', accelerator: 'Alt+Command+I', click: function() { BrowserWindow.getFocusedWindow().toggleDevTools(); } },
+      { label: 'Full Screen', accelerator: 'Alt+Command+F', click: fullscreen }
+    ]
+  }
+];
+
+var menu = Menu.buildFromTemplate(template);
