477d0a02a8aeeadb64b5b1b5b3ae6bbafb91b240
Shoota Kumano <shoota.dev@gmail.com>
2017-04-06 10:49:02 +0900

    photon sample with webview and draggable frame

--- index.js
+++ index.js
@@ -1,51 +1,58 @@
 'use strict';
 
-var electron = require('electron');
-var app = electron.app;
-var BrowserWindow = electron.BrowserWindow;
-var Menu = electron.Menu;
+
+// basic code to launch electron 
+const {app, BrowserWindow} = require('electron')
 var mainWindow = null;
-var remote = require("electron").remote;
 
 app.on('window-all-closed', function() {
-  if (process.platform != 'darwin')
+  if (process.platform != 'darwin') {
     app.quit();
+  }
 });
 
+// App Event:  on ready
 app.on('ready', function() {
-  // メニューをアプリケーションに追加
-  Menu.setApplicationMenu(menu);
-  openWindow();
-});
+  // Menu.setApplicationMenu(menu);
 
-function openWindow() {
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    minWidth: 500,
+    minHeight: 200,
+    acceptFirstMouse: true,
+    titleBarStyle: 'hidden',
+    frame: true
+  });
   mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  // Emitted when the window is closed.
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
-}
+});
 
-function fullscreen(){
-  var w = BrowserWindow.getFocusedWindow();
-  w.setFullScreen(!w.isFullScreen());
-}
+// function fullscreen(){
+//   var w = BrowserWindow.getFocusedWindow();
+//   w.setFullScreen(!w.isFullScreen());
+// }
 
 
-var template = [
-  {
-    label: 'ReadUs',
-    submenu: [{label: 'Quit', accelerator: 'Command+Q', click: function () {app.quit();}}]
-  },
+// var template = [
+//   {
+//     label: 'ReadUs',
+//     submenu: [{label: 'Quit', accelerator: 'Command+Q', click: function () {app.quit();}}]
+//   },
 
-  {
-    label: 'View',
-    submenu: [
-      { label: 'Reload', accelerator: 'Command+R', click: function() { BrowserWindow.getFocusedWindow().reload(); } },
-      { label: 'Toggle DevTools', accelerator: 'Alt+Command+I', click: function() { BrowserWindow.getFocusedWindow().toggleDevTools(); } },
-      { label: 'Full Screen', accelerator: 'Alt+Command+F', click: fullscreen }
-    ]
-  }
-];
+//   {
+//     label: 'View',
+//     submenu: [
+//       { label: 'Reload', accelerator: 'Command+R', click: function() { BrowserWindow.getFocusedWindow().reload(); } },
+//       { label: 'Toggle DevTools', accelerator: 'Alt+Command+I', click: function() { BrowserWindow.getFocusedWindow().toggleDevTools(); } },
+//       { label: 'Full Screen', accelerator: 'Alt+Command+F', click: fullscreen }
+//     ]
+//   }
+// ];
 
-var menu = Menu.buildFromTemplate(template);
+// var menu = Menu.buildFromTemplate(template);
